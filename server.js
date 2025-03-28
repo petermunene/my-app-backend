ae442abc89c6e65d49e55905a79d3a9ee151fb89
petermunene <peter.munene@student.moringaschool.com>
2025-03-28 09:46:18 +0300

    z

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.use(middlewares);
 // ✅ Enable CORS properly
 server.use(
   cors({
-    origin: "https://petermunene.github.io", // Allow your frontend
+    origin: "https://petermunene.github.io/service-card-phase-1-project/", // Allow your frontend
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -23,6 +23,6 @@ server.use(
 server.use("/api", router);
 
 server.listen(process.env.PORT || 5000, () => {
-  console.log("✅ JSON Server is running on port 5000");
+  console.log("JSON Server is running on port 5000");
 });
 
